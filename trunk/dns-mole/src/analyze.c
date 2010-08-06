a4fc13e56986d390b948a8d80c7132b6734ffe29
mario.karuza@gmail.com <mario.karuza@gmail.com@fda4cb6e-1a65-c2ce-2d97-cdb7e20a6977>
2010-08-06 21:02:18 +0000

    detection method

--- trunk/dns-mole/src/analyze.c
+++ trunk/dns-mole/src/analyze.c
@@ -51,112 +51,166 @@ void _analyzer(int fd,short event,void *arg){
                     
 }                    
                     
-void blacklist_method(int num,void *black){
+void blacklist_method(int num_packets,void *black){
 
     moleWorld *blackMole = (moleWorld *) black;
+    int count;
     
-    int cnt;
+    bl_ip *t_bl_ip,*bl_ip_head,*bl_ip_rear;
+    bl_domain *bl_d_head, *bl_d_rear, *t_bl_domain;
+    kdomain *domain_list;
     query *t_query;
-    kdomain *list_domain;
-    ip_list *t_ip,*ip_head,*ip_rear;
-    query_domain *dom,*dom_head,*dom_rear, *dom_helper;
-    domains *t_domains, *t_domains_helper;
-    d_2_ip *domain_ip_list, *domain_ip_list_helper;
-    int domain_count;
-    int t_level = -1;
+    
+    int t_type;
 
-    ip_head = ip_rear = NULL;
-    dom_head = dom_rear =  NULL;
+    bl_d_head = bl_d_rear = NULL;
    
-    for(cnt = 0; cnt < num; cnt++){
+    for(count = 0; count < num_packets; count++){
+        t_type = -1;
         t_query = blackMole->qlist_head;
-        list_domain= search_domain(t_query->dname,blackMole->root_list);
+        domain_list = search_domain(t_query->dname,blackMole->root_list);
 
-        if(list_domain)
-            t_level = list_domain->suspicious;
-
-        if(ip_head == NULL){
-            ip_head = ip_rear = ip_new(t_query->srcip,t_query->dname,t_level);
+        if(domain_list)
+            t_type = domain_list->suspicious;
+        
+            
+        
+        if(bl_ip_head == NULL){
+            bl_ip_head = bl_ip_rear = new_bl_ip(t_query->srcip);
         }
+            
         else{
-            if(!(t_ip = search_ip(ip_head,t_query->srcip))){
-                if((domain_count = return_count(t_ip,t_query->dname)) > 0){
-                    add_count(t_ip,t_query->dname);
+		    if((t_bl_ip = find_ip(bl_ip_head,t_query->srcip)) == 0){
+                t_bl_ip = new_bl_ip(t_query->srcip);
+                bl_ip_rear->next = t_bl_ip;
+                t_bl_ip->prev = bl_ip_rear;
+                bl_ip_rear = t_bl_ip;
+                }
+        }  
+
+        if(t_type == 1)
+            t_bl_ip->black_hosts++;
+        
+        if(t_type == 0)
+            t_bl_ip->white_hosts++;
+        
+        t_bl_ip->all_hosts++;
+
+        if(t_type == -1 || t_type == 1){
+
+            if(bl_d_head == NULL){
+                bl_d_head = bl_d_rear = new_bl_domain(t_query->dname,t_type);
+                add_ip_to_domain(bl_d_rear,(void *)t_bl_ip); 
+            }
+        
+            else{
+                if(!(t_bl_domain = find_domain(bl_d_head,t_query->dname))){
+                    add_ip_to_domain(t_bl_domain,(void *)t_bl_ip);
                 }
                 else{
-                    ip_add_domain(t_ip,t_query->dname,t_level);
+                    t_bl_domain = new_bl_domain(t_query->dname,t_type);
+                    add_ip_to_domain(t_bl_domain,(void *)t_bl_ip);
+                    t_bl_domain->prev = bl_d_rear;
+                    bl_d_rear->next = t_bl_domain;
+		            bl_d_rear = t_bl_domain;
                 }
-            }   
-            else{
-                t_ip = ip_new(t_query->srcip,t_query->dname,t_level);
-                ip_rear->next = t_ip;
-                t_ip->prev = ip_rear;
-                ip_rear = t_ip;
             }
-        }  
-         
-        if(t_level >= 0){
-            t_ip->num++;
-            t_ip->sum += t_level;
         }
 
-        if(dom_head == NULL){
-            dom_head = dom_rear = new_query_domain(t_query->dname);
-        }
-        else{
-            if((dom = find_by_name(dom_head,t_query->dname)) != 0){
-                add_ip_2_domain(dom,(void *)t_ip);
-            }
-            else{
-                dom = new_query_domain(t_query->dname);
-                dom->prev = dom_rear;
-                dom_rear->next = dom;
-                dom_rear = dom;
-            }
-        }
-                
         blackMole->qlist_head = blackMole->qlist_head->next;
         query_remove(t_query);
     }
 
-    float index = 0;
+    float jaccard_index, index;
+    int weight_infected, weight_all;
+    bl_domain *t_domain_1, *t_domain_2;
+    bl_domain_ip *t_domain_ip,*t_ip_for_change;
 
-    for(cnt = 0; cnt < 2; cnt++){
-        dom = dom_head;
-        while(dom){
-            if(dom->type <= 0.1){
-                domain_ip_list = dom->ip;
-                while(domain_ip_list){
-                    domain_count = domain_ip_list->count;
-                    domain_ip_list->ip->num -= domain_count;
-                    ip_remove_domain(domain_ip_list->ip,dom->name);
-                    domain_ip_list = domain_ip_list->next;
+    t_domain_1 = bl_d_head;
+    while(t_domain_1){
+        if(t_domain_1->type == -1){
+            jaccard_index = index = 0.0;
+            weight_infected = weight_all = 0;
+            t_domain_2 = bl_d_head;
+            while(t_domain_2){
+                if(t_domain_2->type != -1){
+                    jaccard_index += calculate_jaccard_index(t_domain_1,t_domain_2);
                 }
-                remove_domain(dom);
-            }
-            else(dom->type == -1 && cnt == 1){
-                index = 0.0;
-                dom_helper = dom_head;
-                while(dom_helper){
-                    if(dom_helper >= 0.9){
-                        domain_ip_list_helper = dom_helper->ip;
-                        index += calculate_blacklist_heuristics(dom_ip_list_helper,dom_ip_list);
-                    }
-                    dom_helper = dom_helper->next;
-                }
-
-                // do something with index;
+            t_domain_2 = t_domain_2->next; 
             }
 
-            dom = dom->next;
+            t_domain_ip = t_domain_1->domain_ip;
+                
+            while(t_domain_ip){
+                if((float)(t_domain_ip->ip->black_hosts / t_domain_ip->ip->all_hosts) > 0.5 )
+                    weight_infected += t_domain_ip->count;
+
+                weight_all += t_domain_ip->count;
+                t_domain_ip = t_domain_ip->next;
+            }
+
+            index = jaccard_index * (weight_infected/weight_all);
+            if( index < 0.2 ){
+
+            // we got some new white list domain for blacklist write that to log
+                
+                load_domain(t_domain_1->d_name,blackMole->re,blackMole->root_list,0);
+                
+                t_ip_for_change = t_domain_1->domain_ip;
+                while(t_ip_for_change){
+                    t_bl_ip = find_ip(bl_ip_head,t_ip_for_change->ip->ip);
+                    t_bl_ip->white_hosts += t_ip_for_change->count;
+                    remove_domain(t_domain_1,0);
+                    t_ip_for_change = t_ip_for_change->next;
+                }
+            }
+            
+            else if( index > 0.8 ){
+
+            // new black domain write log add to structure
+
+                load_domain(t_domain_1->d_name,blackMole->re,blackMole->root_list,1);
+                
+                t_ip_for_change = t_domain_1->domain_ip;
+                while(t_ip_for_change){
+                    t_bl_ip = find_ip(bl_ip_head,t_ip_for_change->ip->ip);
+                    t_bl_ip->black_hosts += t_ip_for_change->count;
+                    t_ip_for_change = t_ip_for_change->next;
+                }
+            }
         }
+
+        t_domain_1 = t_domain_1->next;
     }
 
-   clean_domain_structure(dom_head);
-   clean_ip_structure(ip_head);
+    t_bl_ip = bl_ip_head;
+    
+    while(t_bl_ip){
+        if(((t_bl_ip->black_hosts/t_bl_ip->all_hosts) * (t_bl_ip->white_hosts/t_bl_ip->all_hosts)) <= 0.3){
+
+            // write log that we indetify potential zombie host
+        }
+        t_bl_ip = t_bl_ip->next;
+    }
+
+    remove_domain(bl_d_head,1);
+    remove_ip(bl_ip_head);
 }
 
-float calculate_blacklist_heuristics(void *bad,struct void *good){
+float calculate_jaccard_index(void *unknown,void *black){
 
+    bl_domain *unknown_domain = (bl_domain *) unknown;
+    bl_domain *black_domain = (bl_domain *) black;
+    bl_domain_ip *black_domain_ip = black_domain->domain_ip;
+    int numerator = 0, denominator = 0;
+
+    while(black_domain_ip){
+        if(find_ip_in_domain(unknown_domain->domain_ip,black_domain_ip->ip->ip))
+            numerator++;
+        black_domain_ip = black_domain_ip->next;
+    }
+
+    denominator = black_domain->queried_overall + unknown_domain->queried_overall;
+
+    return (numerator/denominator);
 }
-
